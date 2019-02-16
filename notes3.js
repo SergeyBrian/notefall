85846822d1914335cf5904f0eee71d906d9069c1
Sergio Brianski <s.brianski@yahoo.com>
2019-02-16 22:02:22 +0300

    v1.0.1b.0.0 - Calculating space before key

--- notes3.js
+++ notes3.js
@@ -4,6 +4,7 @@ const s = 1;
 var width = canvas.width = window.innerWidth/s;
 var height = canvas.height = window.innerHeight/s;
 const black = [22, 25, 27, 30, 32, 34, 37, 39, 42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94, 97, 99, 102, 104, 106];
+const white = [21, 23, 23, 24, 26, 28, 29, 31, 33, 35, 36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96, 98, 100, 101, 103, 105, 107, 108];
 
 var vy = -1.5;
 
@@ -11,6 +12,23 @@ const scheme = [["white", "yellow", "red"], ["#65AFFC", "#418BFF", "#4701A6"], [
 var current_scheme = 0;
 const schemes = ["Red orange", "Blue night", "Fading white", "Pink cloud", "Blue peach", "Green fade"];
 
+function calc_x(x){
+    //If it's a black key
+    if (black.indexOf(x) !== -1){
+        //Number of black keys before * width of each key
+        x += (black.indexOf(x) + 1) * 8;
+        x += (white.indexOf(x - 1) + 1) * 10;
+    } else {
+        //If it's a white key
+        //Number of white keys before * width of each key
+        x += (white.indexOf(x) + 1) * 10;
+        for (let i = black.length; black[i] < x; i--){
+            x += (black.indexOf(i) + 1) * 8;
+        }
+    }
+    console.log(x);
+    return x;
+}
 
 window.addEventListener('resize', resize);
 function resize() {
@@ -30,8 +48,8 @@ function createParticle(x){
     grad.addColorStop(1, scheme[current_scheme][2]);
 
     ctx.fillStyle = grad;
-    ctx.fillRect(x*10, height*0.5, w, 10);
-    let p = new Particle(x * 10, height * 0.5, grad, w);
+    ctx.fillRect(calc_x(x), height*0.5, w, 10);
+    let p = new Particle(calc_x(x), height * 0.5, grad, w);
     particles.push(p);
 }
 function endParticle(x){
@@ -41,8 +59,8 @@ function endParticle(x){
     else
         w = 10;
     ctx.fillStyle = "#000";
-    ctx.fillRect(x*10, height*0.5, w, 10);
-    let p = new Particle(x * 10, height * 0.5, "#000", w);
+    ctx.fillRect(calc_x(x), height*0.5, w, 10);
+    let p = new Particle(calc_x(x), height * 0.5, "#000", w);
     particles.push(p);
 }
 
