7a9cdaa4d33a9c426992b4a45c5d283ac1f589c4
Sergio Brianski <s.brianski@yahoo.com>
2019-02-22 16:07:07 +0300

    v1.0.2b.0.0 - Thin black and wide white keys

--- notes3.js
+++ notes3.js
@@ -1,12 +1,11 @@
 const canvas = document.getElementById("canvas"), ctx = canvas.getContext("2d");
 const particles = [];
-const s = 1;
-var width = canvas.width = window.innerWidth/s;
-var height = canvas.height = window.innerHeight/s;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 const black = [22, 25, 27, 30, 32, 34, 37, 39, 42, 44, 46, 49, 51, 54, 56, 58, 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94, 97, 99, 102, 104, 106];
 const white = [21, 23, 23, 24, 26, 28, 29, 31, 33, 35, 36, 38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96, 98, 100, 101, 103, 105, 107, 108];
 
-var vy = -1.5;
+let vy = -1.5;
 
 const scheme = [["white", "yellow", "red"], ["#65AFFC", "#418BFF", "#4701A6"], ["black", "grey", "white"], ["#4E5CEF", "#AB50A8", "#EA4878"], ["#4024D7", "#F32469", "#F6762B"], ["#001E00", "#269926", "#39E639"]];
 let current_scheme = 0;
@@ -20,13 +19,9 @@ function calc_x(x){
     } else {
         //If it's a white key
         //Number of white keys before * width of each key
-        x += (white.indexOf(x) + 1) * 10;
-        if (black.indexOf(x - 1) !== -1)
-            x += black.indexOf(x - 1) * 8;
-        else
-            x += black.indexOf(x - 2) * 8;
+        x = ((white.indexOf(x) + 1) * 10) + (Math.max(black.indexOf(x - 1), black.indexOf(x - 2)) * 8);
     }
-    return x;
+    return x + 100;
 }
 
 window.addEventListener('resize', resize);
@@ -95,8 +90,8 @@ function process(action, note, status){
 
 function success(midi){
     console.log("Midi device connected! ", midi);
-    var inputs = midi.inputs.values();
-    for(var input = inputs.next();
+    let inputs = midi.inputs.values();
+    for(let input = inputs.next();
         input && !input.done;
         input = inputs.next()) {
         input.value.onmidimessage = onMIDIMessage;
